f26f316cbf443ce1b562d8c07e06e5f8969001ea
tHaraldsson <tommy.haraldsson@stud.sti.se>
2025-05-02 13:35:38 +0200

    ADD: made a test for balance check

---
+++ src/main/java/com/project/ATM_simulator/Main.java
@@ -0,0 +1,7 @@
+package com.project.ATM_simulator;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/project/ATM_simulator/ATMTest.java
@@ -0,0 +1,18 @@
+package com.project.ATM_simulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ATMTest {
+
+
+@Test
+    public void testCheckBalance(){
+        Bank bank = new Bank(1000.0);
+        ATM atm = new ATM(bank);
+
+        double balance = atm.checkBalance();
+        assertEquals(1000.0, balance);
+    }
+}
